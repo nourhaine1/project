17e080cc6986b03e13db59e3762534a8ce9a16e8
nourhaine1 <ayadinourhaine@gmail.com>
2021-12-25 19:22:09 +0100

    database

--- web/wie.sql
+++ web/wie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 15 déc. 2021 à 20:25
+-- Généré le : sam. 25 déc. 2021 à 18:21
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -33,8 +33,17 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `avatar` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `avatar`, `description`) VALUES
+(1, 'nourhaine', 'ayadinourhaine@gmail.com', 'nourhaine', 'comp.png', 'Admin de ce site ');
 
 -- --------------------------------------------------------
 
@@ -48,10 +57,21 @@ CREATE TABLE IF NOT EXISTS `client` (
   `email` varchar(255) NOT NULL,
   `name` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `ageEnfant` int(10) UNSIGNED NOT NULL,
   `ville` varchar(255) NOT NULL,
+  `avatar` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id`, `email`, `name`, `password`, `ville`, `avatar`) VALUES
+(12, 'helmi@gmail.com', 'helmi', '147852', 'tunis', '260690657_928203467827419_4538786943553648038_n(1).jpg'),
+(10, 'ayadinorhene@gmail.com', 'nourhaine', '123456', 'bizerte', 'glossy-1293842_1280.png'),
+(13, 'haythem@gmail.com', 'haythem', '123456789haythem', 'ras jebel', 'business-4271251_1280.png'),
+(14, 'walid@gmail.com', 'walid', '12555555555555', 'bizerte', 'exemple back.png'),
+(15, 'safarafrafi@yahoo.com', 'safa', '12345678', 'bizerte', '5197176.jpg');
 
 -- --------------------------------------------------------
 
@@ -67,7 +87,16 @@ CREATE TABLE IF NOT EXISTS `film` (
   `path` varchar(255) NOT NULL,
   `age` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `film`
+--
+
+INSERT INTO `film` (`id`, `nom`, `description`, `path`, `age`) VALUES
+(1, 'film1', 'film pour les enfants d', 'film3.mp4', 4),
+(2, 'film 2', ' film pour les enfants ', 'film2.mp4', 3),
+(3, 'film 3', ' film pour les enfants ', 'film5.mp4', 6);
 
 -- --------------------------------------------------------
 
@@ -83,7 +112,15 @@ CREATE TABLE IF NOT EXISTS `jeu` (
   `path` varchar(255) NOT NULL,
   `age` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `jeu`
+--
+
+INSERT INTO `jeu` (`id`, `name`, `description`, `path`, `age`) VALUES
+(1, 'jeu niveau1', 'des jeux des formes, des couleurs...', 'jeu1.mp4', 3),
+(2, 'jeu niveau 1', 'jeu niveau 1 destinee au enfants ', 'jeux1.mp4', 4);
 
 -- --------------------------------------------------------
 
@@ -99,7 +136,15 @@ CREATE TABLE IF NOT EXISTS `video` (
   `path` varchar(255) NOT NULL,
   `age` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `video`
+--
+
+INSERT INTO `video` (`id`, `name`, `description`, `path`, `age`) VALUES
+(1, 'les chiffres', 'Cours sur les chiffres en arabe', 'video9.mp4', 3),
+(2, 'video educatif 1', 'c\'est un video educatif pour les enfants', 'video3.mp4', 5);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
